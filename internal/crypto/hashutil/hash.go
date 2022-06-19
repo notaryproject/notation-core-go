f836b0e84768fe0e3ab1152137ba16e18694f7cb
Pritesh Bandi <pritesb@amazon.com>
2022-06-19 00:30:21 -0700

    Abstract out signature generation and verification to SignatureEnvelope
    Signed-off-by: Pritesh Bandi <pritesb@amazon.com>

--- internal/crypto/hashutil/hash.go
+++ internal/crypto/hashutil/hash.go
@@ -1,7 +1,11 @@
 // Package hashutil provides utilities for hash.
 package hashutil
 
-import "crypto"
+import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/rsa"
+)
 
 // ComputeHash computes the digest of the message with the given hash algorithm.
 // Callers should check the availability of the hash algorithm before invoking.
@@ -12,4 +16,30 @@ func ComputeHash(hash crypto.Hash, message []byte) ([]byte, error) {
 		return nil, err
 	}
 	return h.Sum(nil), nil
+}
+
+// GetHasher picks up a recommended hashing algorithm for given public keys.
+func GetHasher(pubKey crypto.PublicKey) (crypto.Hash, bool) {
+	var hash crypto.Hash
+	switch key := pubKey.(type) {
+	case *rsa.PublicKey:
+		switch key.Size() {
+		case 256:
+			hash = crypto.SHA256
+		case 384:
+			hash = crypto.SHA384
+		case 512:
+			hash = crypto.SHA512
+		}
+	case *ecdsa.PublicKey:
+		switch key.Curve.Params().BitSize {
+		case 256:
+			hash = crypto.SHA256
+		case 384:
+			hash = crypto.SHA384
+		case 521:
+			hash = crypto.SHA512
+		}
+	}
+	return hash, hash.Available()
 }
---
+++ signer/errors.go
@@ -0,0 +1,103 @@
+package signer
+
+import "fmt"
+
+// SignatureIntegrityError is used when the Signature associated is no longer valid.
+type SignatureIntegrityError struct {
+	err error
+}
+
+func (e SignatureIntegrityError) Error() string {
+	return fmt.Sprintf("signature is invalid. Error: %s", e.err.Error())
+}
+
+// MalformedSignatureError is used when Signature envelope is malformed.
+type MalformedSignatureError struct {
+	msg string
+}
+
+func (e MalformedSignatureError) Error() string {
+	if e.msg != "" {
+		return e.msg
+	}
+	return "signature envelope format is malformed"
+
+}
+
+// UnsupportedSignatureFormatError is used when Signature envelope is not supported.
+type UnsupportedSignatureFormatError struct {
+	mediaType string
+}
+
+func (e UnsupportedSignatureFormatError) Error() string {
+	return fmt.Sprintf("signature envelope format with media type %q is not supported", e.mediaType)
+}
+
+// SignatureNotFoundError is used when signature envelope is not present.
+type SignatureNotFoundError struct{}
+
+func (e SignatureNotFoundError) Error() string {
+	return "signature envelope is not present"
+}
+
+// SignatureAuthenticityError is used when signature is not generated using trusted certificates.
+type SignatureAuthenticityError struct{}
+
+func (e SignatureAuthenticityError) Error() string {
+	return "signature is not produced by a trusted signer"
+}
+
+// UnsupportedOperationError is used when an operation is not supported.
+type UnsupportedOperationError struct {
+	operation string
+}
+
+func (e UnsupportedOperationError) Error() string {
+	return fmt.Sprintf("%q operation is not supported", e.operation)
+}
+
+// UnsupportedSigningKeyError is used when a signing key is not supported
+type UnsupportedSigningKeyError struct {
+	keyType string
+}
+
+func (e UnsupportedSigningKeyError) Error() string {
+	if e.keyType != "" {
+		return fmt.Sprintf("%q signing key is not supported", e.keyType)
+	}
+	return "signing key is not supported"
+}
+
+// MalformedArgumentError is used when an argument to a function is malformed.
+type MalformedArgumentError struct {
+	param string
+	err   error
+}
+
+func (e MalformedArgumentError) Error() string {
+	if e.err != nil {
+		return fmt.Sprintf("%q param is malformed. Error: %s", e.param, e.err.Error())
+	}
+	return fmt.Sprintf("%q param is malformed", e.param)
+}
+
+// MalformedSignRequestError is used when SignRequest is malformed.
+type MalformedSignRequestError struct {
+	msg string
+}
+
+func (e MalformedSignRequestError) Error() string {
+	if e.msg != "" {
+		return e.msg
+	}
+	return "SignRequest is malformed"
+}
+
+// SignatureAlgoNotSupportedError is used when signing algo is not supported.
+type SignatureAlgoNotSupportedError struct {
+	alg string
+}
+
+func (e SignatureAlgoNotSupportedError) Error() string {
+	return fmt.Sprintf("signature algorithm %q is not supported", e.alg)
+}
