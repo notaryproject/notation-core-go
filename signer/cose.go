7ddf30a98b45f3378f44822e34759c359396c6a7
Patrick Zheng <patrickzheng@microsoft.com>
2022-08-03 18:10:44 +0800

    implemented validateIntegrity
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- signer/cose.go
+++ signer/cose.go
@@ -1,12 +1,16 @@
 package signer
 
-import "github.com/veraison/go-cose"
+import (
+	"crypto/x509"
+
+	cosepkg "github.com/veraison/go-cose"
+)
 
 const (
 	MediaTypeCOSE SignatureMediaType = "application/cose"
 )
 
-var signatureAlgCOSEAlgMap = map[SignatureAlgorithm]int{
+var signatureAlgCOSEAlgMap = map[SignatureAlgorithm]int64{
 	RSASSA_PSS_SHA_256: -37,
 	RSASSA_PSS_SHA_384: -38,
 	RSASSA_PSS_SHA_512: -39,
@@ -15,23 +19,23 @@ var signatureAlgCOSEAlgMap = map[SignatureAlgorithm]int{
 	ECDSA_SHA_512:      -36,
 }
 
-var coseAlgSignatureAlgMap = reverseMapCOSE(signatureAlgCOSEAlgMap)
+// var coseAlgSignatureAlgMap = reverseMapCOSE(signatureAlgCOSEAlgMap)
 
-func reverseMapCOSE(m map[SignatureAlgorithm]int) map[int]SignatureAlgorithm {
-	n := make(map[int]SignatureAlgorithm, len(m))
-	for k, v := range m {
-		n[v] = k
-	}
-	return n
-}
+// func reverseMapCOSE(m map[SignatureAlgorithm]int64) map[int64]SignatureAlgorithm {
+// 	n := make(map[int64]SignatureAlgorithm, len(m))
+// 	for k, v := range m {
+// 		n[v] = k
+// 	}
+// 	return n
+// }
 
 // coseEnvelope implements internalSignatureEnvelope interface.
 type coseEnvelope struct {
-	internalEnv *cose.Sign1Message
+	internalEnv *cosepkg.Sign1Message
 }
 
 func newCoseEnvelopeFromBytes(envelopeBytes []byte) (*coseEnvelope, error) {
-	var coseMsg *cose.Sign1Message
+	var coseMsg *cosepkg.Sign1Message
 	err := coseMsg.UnmarshalCBOR(envelopeBytes)
 	if err != nil {
 		return nil, err
@@ -42,7 +46,29 @@ func newCoseEnvelopeFromBytes(envelopeBytes []byte) (*coseEnvelope, error) {
 
 // validateIntegrity implements internalSignatureEnvelope interface
 func (cose *coseEnvelope) validateIntegrity() error {
-	return nil
+	if cose.internalEnv == nil {
+		return SignatureNotFoundError{}
+	}
+	var certs [][]byte
+	if certs, ok := cose.internalEnv.Headers.Unprotected[cosepkg.HeaderLabelX5Chain].([][]byte); !ok || len(certs) == 0 {
+		return MalformedSignatureError{msg: "malformed certificate chain"}
+	}
+
+	cert, err := x509.ParseCertificate(certs[0])
+	if err != nil {
+		return MalformedSignatureError{msg: "malformed leaf certificate"}
+	}
+
+	// verify COSE
+	publicKeyAlg, err := getSignatureAlgorithm(cert)
+	if err != nil || publicKeyAlg == "" {
+		return MalformedSignatureError{msg: "malformed PublicKeyAlgorithm"}
+	}
+	verifier, err := cosepkg.NewVerifier(cosepkg.Algorithm(signatureAlgCOSEAlgMap[publicKeyAlg]), cert.PublicKey)
+	if err != nil {
+		return MalformedSignatureError{msg: "malformed verifier: " + err.Error()}
+	}
+	return cose.internalEnv.Verify(nil, verifier)
 }
 
 // signPayload implements internalSignatureEnvelope interface
