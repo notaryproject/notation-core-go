0f6939f9bc0f052629bd34605e23df89a2cd8af6
Patrick Zheng <patrickzheng@microsoft.com>
2022-08-16 17:03:15 +0800

    update unit tests
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- signature/cose/envelope_test.go
+++ signature/cose/envelope_test.go
@@ -2,6 +2,7 @@ package cose
 
 import (
 	"crypto/x509"
+	"fmt"
 	"reflect"
 	"testing"
 	"time"
@@ -17,7 +18,7 @@ const (
 
 var signingSchemeString = []string{"notary.x509", "notary.x509.signingAuthority"}
 
-func getSignRequest(signingScheme string) (*signature.SignRequest, error) {
+func newSignRequest(signingScheme string) (*signature.SignRequest, error) {
 	certs := getSigningCerts()
 	signer, err := signature.NewLocalSigner(certs, testhelper.GetRSALeafCertificate().PrivateKey)
 	if err != nil {
@@ -37,6 +38,10 @@ func getSignRequest(signingScheme string) (*signature.SignRequest, error) {
 		SigningAgent:  "NotationUnitTest/1.0.0",
 		SigningScheme: signature.SigningScheme(signingScheme),
 	}, nil
+}
+
+func getSignRequest() (*signature.SignRequest, error) {
+	return newSignRequest("notary.x509.signingAuthority")
 }
 
 func getSigningCerts() []*x509.Certificate {
@@ -62,17 +67,173 @@ func TestNewEnvelope(t *testing.T) {
 }
 
 func TestParseEnvelopeError(t *testing.T) {
-	_, err := ParseEnvelope([]byte("Malformed"))
+	var emptyCOSE []byte
+	_, err := ParseEnvelope(emptyCOSE)
+	if err == nil {
+		t.Fatalf("ParseEnvelope() expects signature.MalformedSignatureError, but got nil.")
+	}
+
+	_, err = ParseEnvelope([]byte("Malformed"))
 	if err == nil {
 		t.Fatalf("ParseEnvelope() expects signature.MalformedSignatureError, but got nil.")
 	}
 }
 
+// Tests various scenarios around generating a COSE envelope
+func TestSign(t *testing.T) {
+	env := NewEnvelope()
+	for _, signingScheme := range signingSchemeString {
+		t.Run(fmt.Sprintf("with %s scheme when all arguments are present", signingScheme), func(t *testing.T) {
+			signRequest, err := newSignRequest(signingScheme)
+			if err != nil {
+				t.Fatalf("newSignRequest(). Error = %s", err)
+			}
+			_, err = env.Sign(signRequest)
+			if err != nil {
+				t.Fatalf("sign failed. Error = %s", err)
+			}
+		})
+
+		t.Run(fmt.Sprintf("with %s scheme when minimal arguments are present", signingScheme), func(t *testing.T) {
+			certs := getSigningCerts()
+			signer, err := signature.NewLocalSigner(certs, testhelper.GetRSALeafCertificate().PrivateKey)
+			if err != nil {
+				t.Fatalf("sign failed. Error = %s", err)
+			}
+			signRequest := &signature.SignRequest{
+				Payload: signature.Payload{
+					ContentType: signature.MediaTypePayloadV1,
+					Content:     []byte(TestPayload),
+				},
+				Signer:        signer,
+				SigningTime:   time.Now(),
+				SigningScheme: signature.SigningScheme(signingScheme),
+			}
+			_, err = env.Sign(signRequest)
+			if err != nil {
+				t.Fatalf("sign failed. Error = %s", err)
+			}
+		})
+
+		t.Run(fmt.Sprintf("with %s scheme when expiry is not present", signingScheme), func(t *testing.T) {
+			signRequest, err := newSignRequest(signingScheme)
+			if err != nil {
+				t.Fatalf("newSignRequest(). Error = %s", err)
+			}
+			signRequest.Expiry = time.Time{}
+			_, err = env.Sign(signRequest)
+			if err != nil {
+				t.Fatalf("sign failed. Error = %s", err)
+			}
+		})
+
+		t.Run(fmt.Sprintf("with %s scheme when signing agent is not present", signingScheme), func(t *testing.T) {
+			signRequest, err := newSignRequest(signingScheme)
+			if err != nil {
+				t.Fatalf("newSignRequest(). Error = %s", err)
+			}
+			signRequest.SigningAgent = ""
+			_, err = env.Sign(signRequest)
+			if err != nil {
+				t.Fatalf("sign failed. Error = %s", err)
+			}
+		})
+
+		t.Run(fmt.Sprintf("with %s scheme when extended signed attributes are not present", signingScheme), func(t *testing.T) {
+			signRequest, err := newSignRequest(signingScheme)
+			if err != nil {
+				t.Fatalf("newSignRequest(). Error = %s", err)
+			}
+			signRequest.ExtendedSignedAttributes = nil
+			_, err = env.Sign(signRequest)
+			if err != nil {
+				t.Fatalf("sign failed. Error = %s", err)
+			}
+		})
+	}
+}
+
+// Tests various error scenarios around generating a COSE envelope
+func TestSignErrors(t *testing.T) {
+	env := NewEnvelope()
+	t.Run("when Payload is absent", func(t *testing.T) {
+		signRequest, err := getSignRequest()
+		if err != nil {
+			t.Fatalf("getSignRequest() failed. Error = %v", err)
+		}
+		signRequest.Payload.Content = nil
+		_, err = env.Sign(signRequest)
+		if err == nil {
+			t.Fatalf("Sign() expects error, but got nil.")
+		}
+	})
+
+	t.Run("when PayloadContentType is absent", func(t *testing.T) {
+		signRequest, err := getSignRequest()
+		if err != nil {
+			t.Fatalf("getSignRequest() failed. Error = %v", err)
+		}
+		signRequest.Payload.ContentType = ""
+		_, err = env.Sign(signRequest)
+		if err == nil {
+			t.Fatalf("Sign() expects error, but got nil.")
+		}
+	})
+
+	t.Run("when SigningTime is absent", func(t *testing.T) {
+		signRequest, err := getSignRequest()
+		if err != nil {
+			t.Fatalf("getSignRequest() failed. Error = %v", err)
+		}
+		signRequest.SigningTime = time.Time{}
+		_, err = env.Sign(signRequest)
+		if err == nil {
+			t.Fatalf("Sign() expects error, but got nil.")
+		}
+	})
+
+	t.Run("when Signer is absent", func(t *testing.T) {
+		signRequest, err := getSignRequest()
+		if err != nil {
+			t.Fatalf("getSignRequest() failed. Error = %v", err)
+		}
+		signRequest.Signer = nil
+		_, err = env.Sign(signRequest)
+		if err == nil {
+			t.Fatalf("Sign() expects error, but got nil.")
+		}
+	})
+
+	t.Run("when expiry is before singing time", func(t *testing.T) {
+		signRequest, err := getSignRequest()
+		if err != nil {
+			t.Fatalf("getSignRequest() failed. Error = %v", err)
+		}
+		signRequest.Expiry = signRequest.SigningTime.AddDate(0, 0, -1)
+		_, err = env.Sign(signRequest)
+		if err == nil {
+			t.Fatalf("Sign() expects error, but got nil.")
+		}
+	})
+
+	t.Run("when SigningScheme is absent", func(t *testing.T) {
+		signRequest, err := getSignRequest()
+		if err != nil {
+			t.Fatalf("getSignRequest() failed. Error = %v", err)
+		}
+		signRequest.SigningScheme = ""
+		_, err = env.Sign(signRequest)
+		if err == nil {
+			t.Fatalf("Sign() expects error, but got nil.")
+		}
+	})
+}
+
 func TestSignAndVerify(t *testing.T) {
 	for _, signingScheme := range signingSchemeString {
-		signRequest, err := getSignRequest(signingScheme)
+		signRequest, err := newSignRequest(signingScheme)
 		if err != nil {
-			t.Fatalf("getSignRequest(). Error = %s", err)
+			t.Fatalf("newSignRequest(). Error = %s", err)
 		}
 		env := NewEnvelope()
 		encoded, err := env.Sign(signRequest)
@@ -92,9 +253,9 @@ func TestSignAndVerify(t *testing.T) {
 
 func TestVerifyError(t *testing.T) {
 	for _, signingScheme := range signingSchemeString {
-		SignRequest, err := getSignRequest(signingScheme)
+		SignRequest, err := newSignRequest(signingScheme)
 		if err != nil {
-			t.Fatalf("getSignRequest(). Error = %s", err)
+			t.Fatalf("newSignRequest(). Error = %s", err)
 		}
 		env := NewEnvelope()
 		encoded, err := env.Sign(SignRequest)
@@ -117,9 +278,9 @@ func TestVerifyError(t *testing.T) {
 
 func TestPayloadAndSignerInfo(t *testing.T) {
 	for _, signingScheme := range signingSchemeString {
-		SignRequest, err := getSignRequest(signingScheme)
+		SignRequest, err := newSignRequest(signingScheme)
 		if err != nil {
-			t.Fatalf("getSignRequest(). Error = %s", err)
+			t.Fatalf("newSignRequest(). Error = %s", err)
 		}
 		env := NewEnvelope()
 		encoded, err := env.Sign(SignRequest)
