be20e83ca2dc531e0526dfd3f2a17c49d59ed7d7
Junjie Gao <43160897+JeyJeyGao@users.noreply.github.com>
2022-08-17 09:48:58 +0800

    feat: add jwt sign (#34)
    1. JWT sign replace custom sign
    2. add SigningScheme, crit
    
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>
    
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>
    Co-authored-by: Junjie Gao <junjiegao@microsoft.com>

---
+++ signature/jws/envelope.go
@@ -0,0 +1,211 @@
+package jws
+
+import (
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/notaryproject/notation-core-go/signature"
+	"github.com/notaryproject/notation-core-go/signature/internal/base"
+)
+
+// MediaTypeEnvelope defines the media type name of JWS envelope
+const MediaTypeEnvelope = "application/jose+json"
+
+func init() {
+	if err := signature.RegisterEnvelopeType(MediaTypeEnvelope, NewEnvelope, ParseEnvelope); err != nil {
+		panic(err)
+	}
+}
+
+type envelope struct {
+	internalEnvelope *jwsEnvelope
+}
+
+// NewEnvelope generates an JWS envelope
+func NewEnvelope() signature.Envelope {
+	return &base.Envelope{
+		Envelope: &envelope{},
+	}
+}
+
+// ParseEnvelope parses the envelope bytes and return a JWS envelope
+func ParseEnvelope(envelopeBytes []byte) (signature.Envelope, error) {
+	var e jwsEnvelope
+	err := json.Unmarshal(envelopeBytes, &e)
+	if err != nil {
+		return nil, err
+	}
+	return &base.Envelope{
+		Envelope: &envelope{internalEnvelope: &e},
+		Raw:      envelopeBytes,
+	}, nil
+}
+
+// Sign signs the envelope and return the encoded message
+func (e *envelope) Sign(req *signature.SignRequest) ([]byte, error) {
+	ks, err := req.Signer.KeySpec()
+	if err != nil {
+		return nil, &signature.MalformedSignRequestError{Msg: err.Error()}
+	}
+	alg := ks.SignatureAlgorithm()
+
+	signedAttrs, err := getSignedAttrs(req, alg)
+	if err != nil {
+		return nil, err
+	}
+
+	// JWT sign
+	compact, err := sign(req.Payload.Content, signedAttrs, req.Signer)
+	if err != nil {
+		return nil, &signature.MalformedSignRequestError{Msg: err.Error()}
+	}
+
+	// get certificate chain
+	certs, err := req.Signer.CertificateChain()
+	if err != nil {
+		return nil, err
+	}
+
+	// generate envelope
+	e.internalEnvelope, err = generateJWS(compact, req, certs)
+	if err != nil {
+		return nil, err
+	}
+
+	return json.Marshal(e.internalEnvelope)
+}
+
+// compactJWS converts Flattened JWS JSON Serialization Syntax (section-7.2.2) to
+// JWS Compact Serialization (section-7.1)
+//
+// [RFC 7515]: https://www.rfc-editor.org/rfc/rfc7515.html
+func compactJWS(envelope *jwsEnvelope) string {
+	return strings.Join([]string{
+		envelope.Protected,
+		envelope.Payload,
+		envelope.Signature}, ".")
+}
+
+// Verify checks the validity of the envelope and returns the payload and signerInfo
+func (e *envelope) Verify() (*signature.Payload, *signature.SignerInfo, error) {
+	if e.internalEnvelope == nil {
+		return nil, nil, &signature.SignatureNotFoundError{}
+	}
+
+	if len(e.internalEnvelope.Header.CertChain) == 0 {
+		return nil, nil, &signature.MalformedSignatureError{Msg: "malformed leaf certificate"}
+	}
+
+	cert, err := x509.ParseCertificate(e.internalEnvelope.Header.CertChain[0])
+	if err != nil {
+		return nil, nil, &signature.MalformedSignatureError{Msg: "malformed leaf certificate"}
+	}
+
+	// verify JWT
+	compact := compactJWS(e.internalEnvelope)
+	if err = verifyJWT(compact, cert); err != nil {
+		return nil, nil, err
+	}
+
+	// extract payload
+	payload, err := e.Payload()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// extract signer info
+	signerInfo, err := e.SignerInfo()
+	if err != nil {
+		return nil, nil, err
+	}
+	return payload, signerInfo, nil
+}
+
+// Payload returns the payload of JWS envelope
+func (e *envelope) Payload() (*signature.Payload, error) {
+	if e.internalEnvelope == nil {
+		return nil, &signature.MalformedSignatureError{Msg: "missing jws signature envelope"}
+	}
+	protected, err := parseProtectedHeaders(e.internalEnvelope.Protected)
+	if err != nil {
+		return nil, err
+	}
+	return &signature.Payload{
+		Content:     []byte(e.internalEnvelope.Payload),
+		ContentType: protected.ContentType,
+	}, nil
+}
+
+// SignerInfo returns the SignerInfo of JWS envelope
+func (e *envelope) SignerInfo() (*signature.SignerInfo, error) {
+	if e.internalEnvelope == nil {
+		return nil, &signature.SignatureNotFoundError{}
+	}
+	var signInfo signature.SignerInfo
+
+	// parse protected headers
+	protected, err := parseProtectedHeaders(e.internalEnvelope.Protected)
+	if err != nil {
+		return nil, err
+	}
+	if err := populateProtectedHeaders(protected, &signInfo); err != nil {
+		return nil, err
+	}
+
+	// parse signature
+	sig, err := base64.RawURLEncoding.DecodeString(e.internalEnvelope.Signature)
+	if err != nil {
+		return nil, err
+	}
+	if len(sig) == 0 {
+		return nil, &signature.MalformedSignatureError{Msg: "cose envelope missing signature"}
+	}
+	signInfo.Signature = sig
+
+	// parse headers
+	var certs []*x509.Certificate
+	for _, certBytes := range e.internalEnvelope.Header.CertChain {
+		cert, err := x509.ParseCertificate(certBytes)
+		if err != nil {
+			return nil, err
+		}
+		certs = append(certs, cert)
+	}
+	signInfo.CertificateChain = certs
+	signInfo.UnsignedAttributes.SigningAgent = e.internalEnvelope.Header.SigningAgent
+	signInfo.TimestampSignature = e.internalEnvelope.Header.TimestampSignature
+
+	return &signInfo, nil
+}
+
+// sign the given payload and headers using the given signing method and signature provider
+func sign(payload jwtPayload, headers map[string]interface{}, signer signature.Signer) (string, error) {
+	var privateKey interface{}
+	var signingMethod jwt.SigningMethod
+	if localSigner, ok := signer.(signature.LocalSigner); ok {
+		// local signer
+		alg, err := extractJwtAlgorithm(localSigner)
+		if err != nil {
+			return "", err
+		}
+		signingMethod = jwt.GetSigningMethod(alg)
+
+		// sign with private key
+		privateKey = localSigner.PrivateKey()
+	} else {
+		// remote signer
+		var err error
+		signingMethod, err = newRemoteSigningMethod(signer)
+		if err != nil {
+			return "", err
+		}
+	}
+	// generate token
+	token := jwt.NewWithClaims(signingMethod, payload)
+	token.Header = headers
+
+	return token.SignedString(privateKey)
+}
--- signature/jws/types.go
+++ signature/jws/types.go
@@ -1,6 +1,30 @@
 package jws
 
-import "time"
+import (
+	"time"
+
+	"github.com/notaryproject/notation-core-go/signature"
+)
+
+const (
+	headerKeyAlg                  = "alg"
+	headerKeyCty                  = "cty"
+	headerKeyCrit                 = "crit"
+	headerKeyExpiry               = "io.cncf.notary.expiry"
+	headerKeySigningTime          = "io.cncf.notary.signingTime"
+	headerKeySigningScheme        = "io.cncf.notary.signingScheme"
+	headerKeyAuthenticSigningTime = "io.cncf.notary.authenticSigningTime"
+)
+
+var headerKeys = []string{
+	headerKeyAlg,
+	headerKeyCty,
+	headerKeyCrit,
+	headerKeyExpiry,
+	headerKeySigningTime,
+	headerKeySigningScheme,
+	headerKeyAuthenticSigningTime,
+}
 
 // jwsProtectedHeader contains the set of protected headers.
 type jwsProtectedHeader struct {
@@ -13,33 +37,36 @@ type jwsProtectedHeader struct {
 	// Lists the headers that implementation MUST understand and process.
 	Critical []string `json:"crit,omitempty"`
 
-	// The time at which the signature was generated.
-	SigningTime time.Time `json:"io.cncf.notary.signingTime"`
-
 	// The "best by use" time for the artifact, as defined by the signer.
 	Expiry *time.Time `json:"io.cncf.notary.expiry,omitempty"`
+
+	// Specifies the Notary v2 Signing Scheme used by the signature.
+	SigningScheme signature.SigningScheme `json:"io.cncf.notary.signingScheme"`
+
+	// The time at which the signature was generated. only valid when signing scheme is `notary.x509`
+	SigningTime *time.Time `json:"io.cncf.notary.signingTime,omitempty"`
+
+	// The time at which the signature was generated. only valid when signing scheme is `notary.x509.signingAuthority`
+	AuthenticSigningTime *time.Time `json:"io.cncf.notary.authenticSigningTime,omitempty"`
 
 	// The user defined attributes.
 	ExtendedAttributes map[string]interface{} `json:"-"`
 }
 
-// PayloadContentType list the supported content types for signature's  payload .
-type PayloadContentType string
-
 // jwsUnprotectedHeader contains the set of unprotected headers.
 type jwsUnprotectedHeader struct {
 	// RFC3161 time stamp token Base64-encoded.
-	TimestampSignature []byte `json:"io.cncf.notary.TimestampSignature,omitempty"`
+	TimestampSignature []byte `json:"io.cncf.notary.timestampSignature,omitempty"`
 
 	// List of X.509 Base64-DER-encoded certificates
 	// as defined at https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.6.
 	CertChain [][]byte `json:"x5c"`
 
 	// SigningAgent used for signing
-	SigningAgent string `json:"io.cncf.notary.SigningAgent,omitempty"`
+	SigningAgent string `json:"io.cncf.notary.signingAgent,omitempty"`
 }
 
-type JwsEnvelope struct {
+type jwsEnvelope struct {
 	// JWSPayload Base64URL-encoded.
 	Payload string `json:"payload"`
 
@@ -51,4 +78,29 @@ type JwsEnvelope struct {
 
 	// Base64URL-encoded Signature.
 	Signature string `json:"signature"`
+}
+
+var signatureAlgJWSAlgMap = map[signature.Algorithm]string{
+	signature.AlgorithmPS256: "PS256",
+	signature.AlgorithmPS384: "PS384",
+	signature.AlgorithmPS512: "PS512",
+	signature.AlgorithmES256: "ES256",
+	signature.AlgorithmES384: "ES384",
+	signature.AlgorithmES512: "ES512",
+}
+
+var jwsAlgSignatureAlgMap = reverseMap(signatureAlgJWSAlgMap)
+
+func reverseMap(m map[signature.Algorithm]string) map[string]signature.Algorithm {
+	n := make(map[string]signature.Algorithm, len(m))
+	for k, v := range m {
+		n[v] = k
+	}
+	return n
+}
+
+type jwtPayload []byte
+
+func (jwtPayload) Valid() error {
+	return nil
 }
