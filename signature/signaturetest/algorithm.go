a9d18ee08b8e4270ee16b401c0ab6ca02544dc29
Patrick Zheng <patrickzheng@microsoft.com>
2022-09-14 15:57:09 +0800

    feat: added COSE envelope implementation
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

---
+++ signature/signaturetest/algorithm.go
@@ -0,0 +1,18 @@
+package signaturetest
+
+import "github.com/notaryproject/notation-core-go/signature"
+
+// KeyTypes contains supported key type
+var KeyTypes = []signature.KeyType{signature.KeyTypeRSA, signature.KeyTypeEC}
+
+// GetKeySizes returns the supported key size for the named keyType
+func GetKeySizes(keyType signature.KeyType) []int {
+	switch keyType {
+	case signature.KeyTypeRSA:
+		return []int{2048, 3072, 4096}
+	case signature.KeyTypeEC:
+		return []int{256, 384, 521}
+	default:
+		return nil
+	}
+}
