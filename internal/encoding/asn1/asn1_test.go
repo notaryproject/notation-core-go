d7b0dfd4304ea9b060d5c3a3605be03163c45db4
Junjie Gao <junjiegao@microsoft.com>
2023-12-07 15:41:07 +0800

    test: add unit test
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- internal/encoding/asn1/asn1_test.go
+++ internal/encoding/asn1/asn1_test.go
@@ -14,68 +14,301 @@
 package asn1
 
 import (
-	"encoding/asn1"
+	"fmt"
 	"reflect"
 	"testing"
 )
 
 func TestConvertToDER(t *testing.T) {
-	type data struct {
-		Type  asn1.ObjectIdentifier
-		Value []byte
-	}
+	testData := []struct {
+		name        string
+		ber         []byte
+		der         []byte
+		expectError bool
+	}{
+		{
+			name: "Constructed value",
+			ber: []byte{
+				// Constructed value
+				0x30,
+				// Constructed value length
+				0x2e,
 
-	want := data{
-		Type: asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 2, 1},
-		Value: []byte{
-			0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14,
-			0x9a, 0xfb, 0xf4, 0xc8, 0x99, 0x6f, 0xb9, 0x24,
-			0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
-			0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8, 0x55,
+				// Type identifier
+				0x06,
+				// Type length
+				0x09,
+				// Type content
+				0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01,
+
+				// Value identifier
+				0x04,
+				// Value length in BER
+				0x81, 0x20,
+				// Value content
+				0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14,
+				0x9a, 0xfb, 0xf4, 0xc8, 0x99, 0x6f, 0xb9, 0x24,
+				0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
+				0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8, 0x55,
+			},
+			der: []byte{
+				// Constructed value
+				0x30,
+				// Constructed value length
+				0x2d,
+
+				// Type identifier
+				0x06,
+				// Type length
+				0x09,
+				// Type content
+				0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01,
+
+				// Value identifier
+				0x04,
+				// Value length in BER
+				0x20,
+				// Value content
+				0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14,
+				0x9a, 0xfb, 0xf4, 0xc8, 0x99, 0x6f, 0xb9, 0x24,
+				0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
+				0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8, 0x55,
+			},
+			expectError: false,
+		},
+		{
+			name: "Primitive value",
+			ber: []byte{
+				// Primitive value
+				// identifier
+				0x1f, 0x20,
+				// length
+				0x81, 0x01,
+				// content
+				0x01,
+			},
+			der: []byte{
+				// Primitive value
+				// identifier
+				0x1f, 0x20,
+				// length
+				0x01,
+				// content
+				0x01,
+			},
+			expectError: false,
+		},
+		{
+			name: "Constructed value in constructed value",
+			ber: []byte{
+				// Constructed value
+				0x30,
+				// Constructed value length
+				0x2d,
+
+				// Constructed value identifier
+				0x26,
+				// Type length
+				0x2b,
+
+				// Value identifier
+				0x04,
+				// Value length in BER
+				0x81, 0x28,
+				// Value content
+				0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02,
+				0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14,
+				0x9a, 0xfb, 0xf4, 0xc8, 0x99, 0x6f, 0xb9, 0x24,
+				0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
+				0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8, 0x55,
+			},
+			der: []byte{
+				// Constructed value
+				0x30,
+				// Constructed value length
+				0x2c,
+
+				// Constructed value identifier
+				0x26,
+				// Type length
+				0x2a,
+
+				// Value identifier
+				0x04,
+				// Value length in BER
+				0x28,
+				// Value content
+				0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02,
+				0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14,
+				0x9a, 0xfb, 0xf4, 0xc8, 0x99, 0x6f, 0xb9, 0x24,
+				0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
+				0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8, 0x55,
+			},
+			expectError: false,
+		},
+		{
+			name:        "empty",
+			ber:         []byte{},
+			der:         []byte{},
+			expectError: true,
+		},
+		{
+			name: "identifier high tag number form",
+			ber: []byte{
+				// Primitive value
+				// identifier
+				0x1f, 0xa0, 0x20,
+				// length
+				0x81, 0x01,
+				// content
+				0x01,
+			},
+			der: []byte{
+				// Primitive value
+				// identifier
+				0x1f, 0xa0, 0x20,
+				// length
+				0x01,
+				// content
+				0x01,
+			},
+			expectError: false,
+		},
+		{
+			name: "EOF for identifier high tag number form",
+			ber: []byte{
+				// Primitive value
+				// identifier
+				0x1f, 0xa0,
+			},
+			der:         []byte{},
+			expectError: true,
+		},
+		{
+			name: "EOF for length",
+			ber: []byte{
+				// Primitive value
+				// identifier
+				0x1f, 0xa0, 0x20,
+			},
+			der:         []byte{},
+			expectError: true,
+		},
+		{
+			name: "Unsupport indefinite-length",
+			ber: []byte{
+				// Primitive value
+				// identifier
+				0x1f, 0xa0, 0x20,
+				// length
+				0x80,
+			},
+			der:         []byte{},
+			expectError: true,
+		},
+		{
+			name: "length greater than 4 bytes",
+			ber: []byte{
+				// Primitive value
+				// identifier
+				0x1f, 0xa0, 0x20,
+				// length
+				0x85,
+			},
+			der:         []byte{},
+			expectError: true,
+		},
+		{
+			name: "long form length EOF ",
+			ber: []byte{
+				// Primitive value
+				// identifier
+				0x1f, 0xa0, 0x20,
+				// length
+				0x84,
+			},
+			der:         []byte{},
+			expectError: true,
+		},
+		{
+			name: "length greater > int32",
+			ber: append([]byte{
+				// Primitive value
+				// identifier
+				0x1f, 0xa0, 0x20,
+				// length
+				0x84, 0xFF, 0xFF, 0xFF, 0xFF,
+			}, make([]byte, 0xFFFFFFFF)...),
+			der:         []byte{},
+			expectError: true,
+		},
+		{
+			name: "length greater than content",
+			ber: []byte{
+				// Primitive value
+				// identifier
+				0x1f, 0xa0, 0x20,
+				// length
+				0x02,
+			},
+			der:         []byte{},
+			expectError: true,
+		},
+		{
+			name: "trailing data",
+			ber: []byte{
+				// Primitive value
+				// identifier
+				0x1f, 0xa0, 0x20,
+				// length
+				0x02,
+				// content
+				0x01, 0x02, 0x03,
+			},
+			der:         []byte{},
+			expectError: true,
+		},
+		{
+			name: "EOF in constructed value",
+			ber: []byte{
+				// Constructed value
+				0x30,
+				// Constructed value length
+				0x2c,
+
+				// Constructed value identifier
+				0x26,
+				// Type length
+				0x2b,
+
+				// Value identifier
+				0x04,
+				// Value length in BER
+				0x81, 0x28,
+				// Value content
+				0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02,
+				0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14,
+				0x9a, 0xfb, 0xf4, 0xc8, 0x99, 0x6f, 0xb9, 0x24,
+				0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
+				0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8,
+			},
+			expectError: true,
 		},
 	}
 
-	ber := []byte{
-		// Constructed value
-		0x30,
-		// Constructed value length
-		0x2e,
+	for _, tt := range testData {
+		der, err := ConvertToDER(tt.ber)
+		fmt.Printf("DER: %x\n", der)
+		if !tt.expectError && err != nil {
+			t.Errorf("ConvertToDER() error = %v, but expect no error", err)
+			return
+		}
+		if tt.expectError && err == nil {
+			t.Errorf("ConvertToDER() error = nil, but expect error")
+		}
 
-		// Type identifier
-		0x06,
-		// Type length
-		0x09,
-		// Type content
-		0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01,
-
-		// Value identifier
-		0x04,
-		// Value length in BER
-		0x81, 0x20,
-		// Value content
-		0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14,
-		0x9a, 0xfb, 0xf4, 0xc8, 0x99, 0x6f, 0xb9, 0x24,
-		0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
-		0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8, 0x55,
-	}
-
-	der, err := ConvertToDER(ber)
-	if err != nil {
-		t.Errorf("ConvertToDER() error = %v", err)
-		return
-	}
-
-	var got data
-	rest, err := asn1.Unmarshal(der, &got)
-	if err != nil {
-		t.Errorf("Failed to decode converted data: %v", err)
-		return
-	}
-	if len(rest) > 0 {
-		t.Errorf("Unexpected rest data: %v", rest)
-		return
-	}
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got = %v, want %v", got, want)
+		if !tt.expectError && !reflect.DeepEqual(der, tt.der) {
+			t.Errorf("got = %v, want %v", der, tt.der)
+		}
 	}
 }
