174b51667da0f99630841f6cb711bd463a2672e4
Junjie Gao <junjiegao@microsoft.com>
2024-08-16 07:45:18 +0000

    fix: add comment
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- revocation/internal/crl/errors.go
+++ revocation/internal/crl/errors.go
@@ -3,5 +3,7 @@ package crl
 import "errors"
 
 var (
+	// ErrDeltaCRLNotChecked is returned when the CRL contains a delta CRL but
+	// the delta CRLs are not checked
 	ErrDeltaCRLNotChecked = errors.New("delta CRLs are not checked")
 )
