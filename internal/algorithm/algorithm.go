ff74cc72de8ef679850f40588974f45f9093626d
Patrick Zheng <patrickzheng@microsoft.com>
2024-11-25 18:54:11 +0800

    update
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- internal/algorithm/algorithm.go
+++ internal/algorithm/algorithm.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// package algorithm includes signature algorithms accepted by Notary Project
+// Package algorithm includes signature algorithms accepted by Notary Project
 package algorithm
 
 import (
