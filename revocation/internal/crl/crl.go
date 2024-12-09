630c7a56b5c6e3d8a941694cc1102aaa3438ceaf
Junjie Gao <junjiegao@microsoft.com>
2024-12-09 05:33:00 +0000

    fix: simplify code
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- revocation/internal/crl/crl.go
+++ revocation/internal/crl/crl.go
@@ -30,6 +30,24 @@ import (
 	"golang.org/x/crypto/cryptobyte"
 )
 
+const (
+	// RFC 5280, 5.3.1
+	// CRLReason ::= ENUMERATED {
+	//   unspecified             (0),
+	//   keyCompromise           (1),
+	//   cACompromise            (2),
+	//   affiliationChanged      (3),
+	//   superseded              (4),
+	//   cessationOfOperation    (5),
+	//   certificateHold         (6),
+	//        -- value 7 is not used
+	//   removeFromCRL           (8),
+	//   privilegeWithdrawn      (9),
+	//   aACompromise           (10) }
+	reasonCodeCertificateHold = 6
+	reasonCodeRemoveFromCRL   = 8
+)
+
 var (
 	// oidFreshestCRL is the object identifier for the distribution point
 	// for the delta CRL. (See RFC 5280, Section 5.2.6)
