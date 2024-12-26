a24fe9d1eb5eee957b6e657ed66128acd9d2ff5c
Junjie Gao <junjiegao@microsoft.com>
2024-12-26 08:50:18 +0000

    fix: update comment
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- revocation/internal/crl/crl.go
+++ revocation/internal/crl/crl.go
@@ -30,26 +30,23 @@ import (
 	"golang.org/x/crypto/cryptobyte"
 )
 
+// RFC 5280, 5.3.1
+//
+//	CRLReason ::= ENUMERATED {
+//	  unspecified             (0),
+//	  keyCompromise           (1),
+//	  cACompromise            (2),
+//	  affiliationChanged      (3),
+//	  superseded              (4),
+//	  cessationOfOperation    (5),
+//	  certificateHold         (6),
+//	       -- value 7 is not used
+//	  removeFromCRL           (8),
+//	  privilegeWithdrawn      (9),
+//	  aACompromise           (10) }
 const (
-	// RFC 5280, 5.3.1
-	//
-	//	CRLReason ::= ENUMERATED {
-	//	  unspecified             (0),
-	//	  keyCompromise           (1),
-	//	  cACompromise            (2),
-	//	  affiliationChanged      (3),
-	//	  superseded              (4),
-	//	  cessationOfOperation    (5),
-	//	  certificateHold         (6),
-	//	       -- value 7 is not used
-	//	  removeFromCRL           (8),
-	//	  privilegeWithdrawn      (9),
-	//	  aACompromise           (10) }
-	// certificateHold
-	reasonCodeCertificateHold = 6
-
-	// removeFromCRL
-	reasonCodeRemoveFromCRL = 8
+	reasonCodeCertificateHold = 6 // certificateHold
+	reasonCodeRemoveFromCRL   = 8 // removeFromCRL
 )
 
 var (
