4bd91516e0da2707ea1053e1cceddf838ce8313a
Patrick Zheng <patrickzheng@microsoft.com>
2025-04-22 17:40:34 +0800

    license
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- signature/cose/envelope.go
+++ signature/cose/envelope.go
@@ -10,6 +10,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// Portions (c) 2023 Xenit AB and 2024 The veraison/go-cose Authors.
 
 package cose
 
